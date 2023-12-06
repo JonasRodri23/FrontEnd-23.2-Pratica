5aa583cb07f2d2a78e75861468eb0d9c8e945037
Jonas Rodrigues <jonasrodrigues0213@gmail.com>
2023-12-06 12:41:50 -0300

    Updade 3

--- src/app/components/list-items/list-items.component.ts
+++ src/app/components/list-items/list-items.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './list-items.component.css'
 })
 export class ListItemsComponent {
-  items: string[] = ['Rodrigo', 'Arthur', 'Hugo', 'Dyelson', 'Jonas'];
+  items: { name: string; completed: boolean }[] = [];
+  newItem: string = '';
+
+  addItem() {
+    if (this.newItem.trim() !== '') {
+      this.items.push({ name: this.newItem, completed: false });
+      this.newItem = '';
+    }
+  }
+
+  removeItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  completeItem(index: number) {
+    this.items[index].completed = true;
+  }
 }
